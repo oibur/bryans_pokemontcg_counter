f7da34ebad56e2bcf72ed4cb8912f08e3ae17159
Bryan McGill <bktmcgill@gmail.com>
2021-02-13 22:35:31 -0500

    learning how to json.loads and extract embedded dict from list

--- testing.py
+++ testing.py
@@ -1,7 +1,22 @@
 import json
 
-base_set = open('cards\Base.json','r')
-cards = base_set.read()
+with open('cards\Base.json') as base_set:
+    cards = json.load(base_set)
 
-obj = json.loads(cards)
-print(obj['name'])
+card_dict = cards[0]
+
+for _ in card_dict:
+    print(card_dict["name"])
+
+#import the data to be used as list
+    #using import json
+    #using external API
+
+#repeat for all json files
+    #can I do all at once, or write a function to loop throuhg files?
+
+#modify the list down to just ["name":"nationalPokedexNumber"]
+
+#count the occurance of each pokemon and return sorted list
+
+#Visualize the data in a graph sorted by Dex#
