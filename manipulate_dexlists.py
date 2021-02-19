e55a235b2bf0f75474c2b489b1187ac9ca0ea267
unknown <bktmcgill@gmail.com>
2021-02-19 10:50:56 -0500

    deleted produced list for user to experience it being produced

--- manipulate_dexlists.py
+++ manipulate_dexlists.py
@@ -3,11 +3,9 @@ import os
 
 from compile_dexlists import master_dexlist as md
 
-set_names = [pos_json for pos_json in os.listdir() if pos_json.endswith('.json')]
-
-data = md(set_names)
-
 occurences = dict()
+set_names = [pos_json for pos_json in os.listdir() if pos_json.endswith('.json')]
+data = md(set_names)
 
 def ta_da(information):
     for dexnum in information: 
